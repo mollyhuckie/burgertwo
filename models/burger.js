05239234ffde8c99767575bed87dfbfa95284cdc
Molly Huckie <mollyhuckie@Mollys-MacBook-Pro.local>
2017-08-11 19:45:09 -0400

    check in 2

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,40 @@
-var orm = require("../config/orm.js");
-console.log(orm);
-//create the code that will call the ORM functions using burger specific input for the ORM
-orm.selectAll("burgers", function(results){
-    console.log(results);
-});
+var Sequelize = require("sequelize"); 
 
-var burger = {
-    all: function(callback){
-        orm.selectAll("burgers", function(results){
-        callback(results);
-        });
+
+module.exports = function(sequelize) {
+  var burger = sequelize.define("burgers", {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    burger_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    devoured: {
+      type: Sequelize.BOOLEAN,
+    },
+    date: {
+      type: Sequelize.DATE,
     }
+  });
+  return burger;
 };
 
-module.exports = burger;
+// var orm = require("../config/orm.js");
+// console.log(orm);
+// //create the code that will call the ORM functions using burger specific input for the ORM
+// orm.selectAll("burgers", function(results){
+//     console.log(results);
+// });
+
+// var burger = {
+//     all: function(callback){
+//         orm.selectAll("burgers", function(results){
+//         callback(results);
+//         });
+//     }
+// };
+
+//module.exports = burgers;
